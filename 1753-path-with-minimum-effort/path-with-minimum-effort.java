05404fdbc4b8c41ee735b504d66ab046674fca8b
bhargav789645 <157564110+bhargav789645@users.noreply.github.com>
2025-10-06 23:38:27 +0530

    Time: 37 ms (94.08%) | Memory: 45.6 MB (23.80%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,48 @@
+class Pair{
+    int distance;
+    int row;
+    int col;
+    public Pair(int distance,int row,int col){
+        this.distance = distance;
+        this.row = row;
+        this.col = col;
+    }
+}
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)
+        -> a.distance - b.distance);
+        int n = heights.length;
+        int m = heights[0].length;
+        int[][] dist = new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        dist[0][0] = 0;
+        pq.offer(new Pair(0,0,0));
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
+        while(pq.size() !=0){
+            Pair it = pq.peek();
+            pq.remove();
+            int diff = it.distance;
+            int row = it.row;
+            int col = it.col;
+            if(row== n-1 && col == m-1) return diff;
+            for(int i=0;i<4;i++){
+                int newr = row + dr[i];
+                int newc = col + dc[i];
+                if(newr >= 0 && newc >= 0 && newr < n && newc <m){
+                    int newEffort = Math.max(Math.abs(heights[row][col] - heights[newr][newc]),diff);
+                    if(newEffort < dist[newr][newc]){
+                        dist[newr][newc] = newEffort;
+                        pq.offer(new Pair(newEffort, newr, newc));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
