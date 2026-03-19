76a9b937fe8970ef983dd1bb169edea70f4e54ed
bhargav789645 <157564110+bhargav789645@users.noreply.github.com>
2026-03-19 23:03:21 +0530

    Time: 6 ms (65.60%) | Memory: 161.3 MB (58.40%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -2,26 +2,30 @@ class Solution {
     public int countSubmatrices(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        if(grid[0][0] > k) return 0;
         int count = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-               if(i==0 && j==0){
-                grid[0][0] = grid[0][0];
-            }
-               else if(i==0) {
-                grid[i][j] = grid[i][j-1] + grid[i][j];
-               }
-               else if(j==0) {
-                 grid[i][j] = grid[i-1][j] + grid[i][j];
-               }
-               else{
-                grid[i][j] = grid[i][j] + grid[i-1][j] + grid[i][j-1] - grid[i-1][j-1];
-               }
-               if(grid[i][j] <= k) count++;
-            
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+
+                if(i == 0 && j == 0){
+                    // no change
+                }
+                else if(i == 0){
+                    grid[i][j] += grid[i][j - 1];
+                }
+                else if(j == 0){
+                    grid[i][j] += grid[i - 1][j];
+                }
+                else{
+                    grid[i][j] += grid[i - 1][j] 
+                               + grid[i][j - 1] 
+                               - grid[i - 1][j - 1];
+                }
+
+                if(grid[i][j] <= k) count++;
             }
         }
+
         return count;
     }
 }
